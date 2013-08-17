27e35a6e314818f4db6539f52ce3f92b0a0f4b93
Al <alvarosdpv@alvarosdpv-VirtualBox.(none)>
2013-08-17 12:57:56 +0200

    Updated web.js to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!\nA.M.D.G');
+  fs.readFile('index.html', function( err, data) {
+    if (err) throw err;
+    var pageString = data.toString();
+    response.send(pageString);
+  }
 });
 
 var port = process.env.PORT || 5000;
